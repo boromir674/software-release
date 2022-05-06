eb141e607ca71c83a00da48d1bc7dd750890f0fa
konstantinos <boromir674@hotmail.com>
2022-05-07 01:03:11 +0300

    read repo directory as input from user through the cli

--- src/software_release/cli.py
+++ src/software_release/cli.py
@@ -3,36 +3,35 @@ from time import sleep
 import click
 
 from .app_commands import *
-# @click.argument('content_image')
-# @click.argument('style_image')
-# @click.option('--iterations', '-it', type=int, default=100, show_default=True)
-# @click.option('--location', '-l', type=str, default='.')
+from .repository_factory import RepositoryFactory
+from .release_wizard import ReleaseWizard
+
+
+@click.option('--path', '-p', 'repo_path', type=str, default='.')
 @click.command()
-def cli():
+def cli(repo_path: str):
 
-    from .repository_factory import RepositoryFactory
-    from .release_wizard import ReleaseWizard
-    REPO_PATH = '/data/repos/software-patterns'
-
-    repo = RepositoryFactory.create(REPO_PATH)
+    repo = RepositoryFactory.create(repo_path)
 
     release_wizard = ReleaseWizard.create(repo, [
         'welcome-to-app',
         'sleep_0.5',
+
         'welcome-to-wizard-node',
         'active-branch-check',  # if we are NOT on a branch named 'release', exit
-        
         'sleep_0.35',
+
         'determine-new-version',
-# pull master branch
+        # 'pull-branch-with-releases',  # ie pull master branch
         # 'sleep_0.35',
+
         # 'version-bump',
         # 'sleep_0.35',
+
         # 'branch-references',
-
-        # 'changelog',
-
+        'changelog',
         # 'sleep_0.35',
+
         # 'push-active-branch',
 
         # 'sleep_0.35',
@@ -43,9 +42,9 @@ def cli():
 
         # 'wait-for-pr-approval',
         
-        'pull-branch-with-releases',  # pull master branch
-        # then modify above since we do not checkout master
-        'tag-commit-on-branch-with-releases',
+        # 'pull-branch-with-releases',  # pull master branch
+
+        # 'tag-commit-on-branch-with-releases',
         # 'push-tag',
     ])
 
