2fcfe229e35c3c6af596238dd8e39db6ee0c25c7
konstantinos <boromir674@hotmail.com>
2023-10-18 01:13:39 +0300

    refactor(commit-message-parser): make code more clear & add docstrings

--- src/software_release/angular_parser.py
+++ src/software_release/angular_parser.py
@@ -1,8 +1,8 @@
-"""
-Angular commit style parser
+"""Provides a Commit Parser following the Angular Style Specification.
 
 https://github.com/angular/angular/blob/master/CONTRIBUTING.md#-commit-message-guidelines
 """
+import typing as t
 import logging
 import re
 from typing import List
@@ -49,15 +49,18 @@ TYPES = OrderedDict((  # alias (on git message) -> human readable label
     ('chore', 'chore'),
     ('revert', 'revert'),
     ('improvement', 'improvement'),
+
+    ('dev-container', 'dev-container'),
+    # frontend specific
+    ('storybook', 'storybook'),
+
 ))
 
-re_parser = re.compile(
-    r"(?P<type>" + "|".join(TYPES.keys()) + ")"
+regex_components = (
     r"(?:\((?P<scope>[^\n]+)\))?"
     r"(?P<break>!)?: "
     r"(?P<subject>[^\n]+)"
-    r"(:?\n\n(?P<text>.+))?",
-    re.DOTALL,
+    r"(:?\n\n(?P<text>.+))?"
 )
 
 MINOR_TYPES = [
@@ -70,14 +73,37 @@ PATCH_TYPES = [
 ]
 
 
-def parse_commit_message(message: str) -> ParsedCommit:
-    """
-    Parse a commit message according to the angular commit guidelines specification.
+def parse_commit_message(message: str, strict=False) -> ParsedCommit:
+    """Parse a commit message according to the angular commit guidelines specification.
 
-    :param message: A string of a commit message.
-    :return: A tuple of (level to bump, type of change, scope of change, a tuple with descriptions)
-    :raises UnknownCommitMessageStyleError: if regular expression matching fails
+    https://github.com/angular/angular/blob/master/CONTRIBUTING.md#-commit-message-guidelines
+
+    Determines the minimum version bump that should be applied, judging
+    by examining the successfully parsed commit messages.
+
+    Args:
+        message (str): The commit message to parse.
+        strict (bool): If True, parse only messages if their 'type' is known to the precompiled list of TYPES.
+    
+    Returns:
+        ParsedCommit: A tuple containing the minimum version bump, the type of
+            the commit, the scope of the commit, and the description of the
+            commit.
+    
+    Raises:
+        UnknownCommitMessageStyleError: if regular expression match fails
     """
+    types: t.List[str] = list(TYPES.keys())
+    if not strict:
+        types.append('\w[\w\-_]*')
+    re_parser = re.compile(
+        r"(?P<type>" + "|".join(types) + ")"
+        r"(?:\((?P<scope>[^\n]+)\))?"
+        r"(?P<break>!)?: "
+        r"(?P<subject>[^\n]+)"
+        r"(:?\n\n(?P<text>.+))?",
+        re.DOTALL,  # make dot match newline
+    )
     # Attempt to parse the commit message with a regular expression
     parsed = re_parser.match(message)
     if not parsed:
@@ -85,27 +111,28 @@ def parse_commit_message(message: str) -> ParsedCommit:
             "Unable to parse the given commit message: {}".format(message)
         )
 
+    # descriptions have 1 or more elements: 1st being subject and each of the
+    # rest correspond to a text paragraph
+    descriptions_list: List[str] = [parsed.group("subject")]
     if parsed.group("text"):
-        descriptions = parse_paragraphs(parsed.group("text"))
-    else:
-        descriptions = list()
-    # Insert the subject before the other paragraphs
-    descriptions.insert(0, parsed.group("subject"))
+        descriptions_list.extend(list(parse_paragraphs(parsed.group("text"))))
 
     # Check for mention of breaking changes
+    commit_change_type = parsed.group("type")
     level_bump = 0
-    if parsed.group("break") or any([re_breaking.match(p) for p in descriptions]):
+    if parsed.group("break") or any([re_breaking.match(p) for p in descriptions_list]):
         level_bump = 3  # Major
-
     # Set the bump level based on commit type
-    if parsed.group("type") in MINOR_TYPES:
+    elif commit_change_type in MINOR_TYPES:
         level_bump = max([level_bump, 2])
-
-    if parsed.group("type") in PATCH_TYPES:
+    elif commit_change_type in PATCH_TYPES:
         level_bump = max([level_bump, 1])
 
     return ParsedCommit(
-        level_bump, TYPES[parsed.group("type")], parsed.group("scope"), descriptions,
+        level_bump,
+        TYPES.get(commit_change_type, commit_change_type),
+        parsed.group("scope"),
+        descriptions_list,
     )
 
 
