7f6da5e9bc7e6aa24fe1c4ad1fe4a6fc6b9e2ac6
konstantinos <boromir674@hotmail.com>
2023-10-18 01:57:54 +0300

    clean code

--- src/software_release/nodes/node_components/make_gh_release.py
+++ src/software_release/nodes/node_components/make_gh_release.py
@@ -1,11 +1,6 @@
 import typing as t
 from software_release.nodes.node import Node
 
-"""
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer <YOUR-TOKEN>" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-"""
 
 @Node.register_as_subclass('make-gh-release')
 class MakeGithubRelease(Node):
@@ -15,8 +10,7 @@ class MakeGithubRelease(Node):
 
         github_releases: t.List[t.Dict[str, t.Any]] = cls.cmd('get-github-releases',
             request.repository.org_name, # owner, github username
-            # request.repository.name, # repository name as seen in github
-            'cookiecutter-python-package',
+            request.repository.name, # repository name as seen in github
             limit=3,
         )
         # print(f"Github Releases: {github_releases}")
@@ -24,18 +18,26 @@ class MakeGithubRelease(Node):
         is_prerelease = request.new_version.is_prerelease()
         print(f"[DEBUG] is prerelase ? {is_prerelease}")
 
-        # tag_str = request.tag_reference.name
-        tag_str = "v1.3.1-dev"
-        # cls.cmd('create-github-release', {
-        #     # "tag_name": request.tag_reference.name,
-        #     "tag_name": "v1.3.1-dev",
-        #     # "target_commitish": "main",
-        #     "name": f"{request.repository.name} {tag_str} Release",
-        #     # "body": "Description of the release",
-        #     "draft": False,
-        #     "prerelease": is_prerelease,
-        #     "generate_release_notes": True,
-        # })
+        tag_str = request.tag_reference.name
+        # tag_str = "v1.3.1-dev"
+
+        # Ask user if Release is Draft
+        dialog = cls.dialog('select-gh-release-info')
+        is_draft: bool = dialog.dialog({
+            'options': [
+                'Is DRAFT',
+                'is NOT Draft',
+        ]}) == 'Is DRAFT'
+
+        cls.cmd('create-github-release', {
+            "tag_name": request.tag_reference.name,
+            # "target_commitish": "main",
+            "name": f"{request.repository.name} {tag_str} Release",
+            # "body": "Description of the release",
+            "draft": is_draft,
+            "prerelease": is_prerelease,
+            "generate_release_notes": True,
+        })
 
     def handle(self, request):
         self._handle(request)
